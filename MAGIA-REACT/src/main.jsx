8d95210c17f549a7a4784be18ac67fe45ba42342
franciscoj <frankmazta@gmail.com>
2025-03-12 19:08:56 -0400

    añade componente Lista en main.jsx para mostrar nombres

--- MAGIA-REACT/src/main.jsx
+++ MAGIA-REACT/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {App,Lista} from './App.jsx'
+// import Lista from './components/Lista.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Lista/>
   </StrictMode>,
 )
