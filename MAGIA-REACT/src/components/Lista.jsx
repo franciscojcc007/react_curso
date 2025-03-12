69d3b0e54d9d2dc08a7d9d9816f62c6aad87bba7
franciscoj <frankmazta@gmail.com>
2025-03-12 19:09:03 -0400

    añade esqueleto del componente Lista en Lista.jsx

---
+++ MAGIA-REACT/src/components/Lista.jsx
@@ -0,0 +1,14 @@
+
+
+// function Lista() {
+//   const nombres = ['Ana', 'Juan', 'Pedro','Francisco'];
+
+//   return (
+//     <ul>
+//       {nombres.map((nombre, index) => (
+//         <li key={index}>{nombre}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+// export default Lista
