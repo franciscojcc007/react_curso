cfcd3a3382a1cf82b72d276aaca98fbdf2a57a54
franciscoj <frankmazta@gmail.com>
2025-03-18 20:05:17 -0400

    añade el componente Presentation con contenido inicial y estilos asociados

---
+++ MAGIA-REACT/src/components/Presentation.jsx
@@ -0,0 +1,15 @@
+import '../style/presentation.css'
+export const Presentation =()=> {
+  return (
+    <>
+      <h1>curso comenzado por frank</h1>
+      <div className='card'>
+        <p>
+          primeros pasos en react
+        </p>
+      </div>
+    </>
+  )
+}
+
+
