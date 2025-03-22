e9f7cecd7bed6ec5722dd9893174fa6a12e1024b
franciscoj <frankmazta@gmail.com>
2025-03-22 11:07:00 -0400

    actualiza main.jsx para usar el componente List y añade botones con iconos

--- MAGIA-REACT/src/main.jsx
+++ MAGIA-REACT/src/main.jsx
@@ -2,11 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import {Presentation} from './components/Presentation.jsx'
-import {Lista} from './components/Lista.jsx'
+import {List} from './components/List.jsx'
+// import { DiAtom } from 'react-icons/di'
+import {Button} from './components/Button.jsx'
+import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa'
+import React from 'react'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Presentation />
-    <Lista/>
+    {/* <DiAtom /> */}
+    <List/>
+    <React.Fragment>
+      <Button text="Crear" icon={FaPlus} />
+      <Button text="Editar" icon={FaEdit} />
+      <Button text="Eliminar" icon={FaTrash} />
+    </React.Fragment>
   </StrictMode>,
 )
