0d28676a525df8b90c9ce9e16051bde92679fa0d
franciscoj <frankmazta@gmail.com>
2025-03-30 12:44:27 -0400

    se agrega el componente TaskApp para gestionar tareas con funcionalidades de agregar y eliminar

---
+++ MAGIA-REACT/src/components/TaskApp.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+// import { Button } from "./button/Button.jsx";
+import { Form } from "./form/form.jsx";
+import { List } from "./list/List.jsx";
+import { Presentation } from './presentations/Presentation.jsx'
+// import { FaPlus, FaEdit, FaTrash } from "react-icons/fa";
+
+const TaskApp = () => {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+  // Agregar tarea
+  const handleAddTask = () => {
+    if (currentTask.trim() === "") return;
+    setTasks([...tasks, currentTask]);
+    setCurrentTask(""); // Limpia el input
+  };
+
+  // Eliminar tarea
+  const handleDeleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <Presentation />
+      <h1>Gestor de Tareas</h1>
+      <Form value={currentTask} onChange={setCurrentTask} onAddTask={handleAddTask} />
+      <List tasks={tasks} onDeleteTask={handleDeleteTask} />
+    </div>
+  );
+};
+
+export { TaskApp };
