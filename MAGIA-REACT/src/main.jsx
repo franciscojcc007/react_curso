4df1d9e80e63fdd8d62b58879ba27100be9e118c
franciscoj <frankmazta@gmail.com>
2025-03-30 12:44:13 -0400

    se actualiza la estructura del componente principal y se reemplaza por TaskApp

--- MAGIA-REACT/src/main.jsx
+++ MAGIA-REACT/src/main.jsx
@@ -1,24 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/style.css'
-import {Presentation} from './components/presentations/Presentation.jsx'
-import {List} from './components/list/List.jsx'
-import {Button} from './components/button/Button.jsx'
-import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa'
+// import { Presentation } from './components/presentations/Presentation.jsx'
+// import { List } from './components/list/List.jsx'
+// import { Button } from './components/button/Button.jsx'
+// import { Form } from './components/form/form.jsx'
+import { TaskApp } from './components/TaskApp.jsx'
+
+// import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa'
 import React from 'react'
-// import { DiAtom } from 'react-icons/di'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Presentation />
-    {/* <DiAtom /> */}
-    <List/>
+    <TaskApp/>
+    {/* <Presentation /> */}
+    {/* <Form />
+    <List />
     <React.Fragment>
-      <Button text="Crear" icon={FaPlus} />
-      <Button text="Editar" icon={FaEdit} />
-      <Button text="Eliminar" icon={FaTrash} />
-    </React.Fragment>
+      <Button text="Crear" icon={FaPlus} action='create' />
+      <Button text="Editar" icon={FaEdit} action='edit' />
+      <Button text="Eliminar" icon={FaTrash} action='delete' />
+    </React.Fragment> */}
   </StrictMode>,
 )
+
 
