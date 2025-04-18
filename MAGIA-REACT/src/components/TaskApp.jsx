8943a7386de9a22e23527e4465c0ed4bef63043c
franciscoj <frankmazta@gmail.com>
2025-04-18 18:05:16 -0400

    se actualiza el valor inicial del componente Contador a 1 y se eliminan importaciones comentadas para limpiar el código

--- MAGIA-REACT/src/components/TaskApp.jsx
+++ MAGIA-REACT/src/components/TaskApp.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
-// import { Button } from "./button/Button.jsx";
 import { Form } from "./form/form.jsx";
 import { List } from "./list/List.jsx";
 import { Presentation } from './presentations/Presentation.jsx'
 import { Contador } from  "./contador/Contador.jsx"
-// import { FaPlus, FaEdit, FaTrash } from "react-icons/fa";
 
-const TaskApp = () => {
+
+export const TaskApp = () => {
   const [tasks, setTasks] = useState([]);
   const [currentTask, setCurrentTask] = useState("");
 
-  // Agregar tarea
   const handleAddTask = () => {
     if (currentTask.trim() === "") return;
     setTasks([...tasks, currentTask]);
-    setCurrentTask(""); // Limpia el input
+    setCurrentTask("");
   };
 
-  // Eliminar tarea
   const handleDeleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
@@ -25,12 +22,11 @@ const TaskApp = () => {
   return (
     <>
       <Presentation />
-      <h1>Gestor de Tareas</h1>
       <Form value={currentTask} onChange={setCurrentTask} onAddTask={handleAddTask} />
       <List tasks={tasks} onDeleteTask={handleDeleteTask} />
-      <Contador value={100}/>
+      <Contador value={1}/>
     </>
   );
 };
 
-export { TaskApp };
+
