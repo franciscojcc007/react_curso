c24e1237106dc31414d3b1c97db06309beb9df62
franciscoj <frankmazta@gmail.com>
2025-04-18 18:04:52 -0400

    se eliminan importaciones comentadas en main.jsx para limpiar el código

--- MAGIA-REACT/src/main.jsx
+++ MAGIA-REACT/src/main.jsx
@@ -1,27 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/style.css'
-// import { Presentation } from './components/presentations/Presentation.jsx'
-// import { List } from './components/list/List.jsx'
-// import { Button } from './components/button/Button.jsx'
-// import { Form } from './components/form/form.jsx'
-import { TaskApp } from './components/TaskApp.jsx'
-
-// import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa'
+import {TaskApp}  from './components/TaskApp.jsx'
 import React from 'react'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TaskApp/>
-    {/* <Presentation /> */}
-    {/* <Form />
-    <List />
-    <React.Fragment>
-      <Button text="Crear" icon={FaPlus} action='create' />
-      <Button text="Editar" icon={FaEdit} action='edit' />
-      <Button text="Eliminar" icon={FaTrash} action='delete' />
-    </React.Fragment> */}
   </StrictMode>,
 )
 
